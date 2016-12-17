4f46bd9465e3e50cb04c51ea9fe93e6d884c36cc
vgmoose <me@vgmoose.com>
2016-12-17 16:44:05 -0500

    Initial Commit

---
+++ netcat-client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  netcat-client
+//
+//  Created by vgm on 12/17/16.
+//  Copyright © 2016 vgmoose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
